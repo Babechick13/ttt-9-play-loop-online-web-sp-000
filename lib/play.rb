f3762c1ec2a39ef89519e21dfab4a2d5157f170f
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-19 00:09:24 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -6,23 +6,18 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-
 def move(board, index, current_player = "X")
   board[index] = current_player
 end
-
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
-
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
-
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
@@ -34,9 +29,6 @@ def turn(board)
     turn(board)
   end
 end
-
-# Define your play method below
-
 
 # Define your play method below
 def play(board)
